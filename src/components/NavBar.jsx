13c41dfa8752e36fa3e4811e94efa4283c6e3e6f
Donnie Paxi <donypaxi@gmail.com>
2026-06-08 22:40:31 -0500

    correción del logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import LOGO from "../assets/img/LOGO.png";
+import LOGO from "../assets/img/logo.png";
 
 export const Navbar = () => {
   const [menuAbierto, setMenuAbierto] = useState(false);
